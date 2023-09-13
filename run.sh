9db6e272cedf5bbbbbc74b8716447254da6f2582
barbariansubhkaran <subhkaran.foam@gmail.com>
2023-09-13 17:20:21 +0530

    case

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+decomposePar -force -allRegions
+
+faceAgglomerate -noFunctionObjects -region Fluid
+
+foamJob -parallel -screen faceAgglomerate -noFunctionObjects -region Fluid
+
+viewFactorsGen -noFunctionObjects -region Fluid
+
+foamJob -parallel -screen  viewFactorsGen -noFunctionObjects -region Fluid
+
+foamJob -parallel -screen chtMultiRegionSimpleFoam
